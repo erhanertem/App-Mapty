7aba7b5ae95692913fb45ff0870c4c431d0b75ae
Erhan Ertem <eertem@gmail.com>
2022-09-23 02:08:18 +0300

    Establish the preliminary relationship between the form and leaflet click

--- script.js
+++ script.js
@@ -11,57 +11,70 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+//GLOBAL VARIABLES
+let map, leafletEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
+    //-->GEO API SUCCESS CALL-BACK FUNCTION
     function (position) {
       // console.log(position);
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       // console.log(latitude, longitude);
-
+      //-->GET GEOLOCATION FROM GEO WEB API
       const coords = [latitude, longitude];
-
       // console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
-
-      const map = L.map('map').setView(coords, 13);
+      //-->LEAFLET RENDER MAP PER COORDS
+      map = L.map('map').setView(coords, 13);
       // console.log(map);
-
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-
       //EVENTHANDLER BUILT-IN LEAFLET
-      map.on('click', function (leafletEvent) {
-        // console.log(leafEvent);
-        const { lat, lng } = leafletEvent.latlng;
-
-        L.marker([lat, lng], {
-          opacity: 0.75,
-          riseOnHover: true,
-          riseOffset: 1,
-        })
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 150,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
+      map.on('click', function (lEvent) {
+        //-->SHOW THE FORM UPON CLICK ON THE MAP
+        leafletEvent = lEvent; //Assign as global variable
+        form.classList.remove('hidden'); //reveal the input form
+        inputDistance.focus(); //by default focus on distance
       });
-    }, //SUCCESS FUNCTION
+    },
+    //-->GEO API ERROR CALL-BACK FUNCTION
     function () {
       alert('Could not get your location');
-    }, //ERROR FUNCTION
+    },
+    //-->GEO API OPTIONS
     {
       enableHighAccuracy: true, //WHEN POSSIBLE GET THE BEST RESULT
       timeout: 5000, //WAIT FOR XXX mls TO RETURN LOCATION @MAX
       maximumAge: 0, //RETURN REAL POSITION NOT CACHED POSITION
-    } //OPTIONS
+    }
   );
 }
+//EVENTHANDLER WORKOUT SUBMIT FORM
+form.addEventListener('submit', function (e) {
+  e.preventDefault(); //to disable auto submit
+  //-->CLEAR INPUT FIELDS ON THE FORM
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  //-->DISPLAY THE LEAFLET MARKER
+  console.log(leafletEvent);
+  const { lat, lng } = leafletEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup', //Override custom CSS provided by us on top of the leaflet.css
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
