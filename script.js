efb3ea59c7e58c99bf4d829f6a403169788819ad
Erhan Ertem <eertem@gmail.com>
2022-09-23 10:29:49 +0300

    Fill in newWorkout() and fix leaflet eventhandler call-back function  by bind(this)

--- script.js
+++ script.js
@@ -21,13 +21,26 @@ class App {
 
   constructor() {
     //-->GET THE POSITION OF THE USER
-    this._getPosition(); //NOTE: CONSTRUCTOR FUNCTION IS EXECUTED THE MOMENT AN INSTANCE OF APP IS CREATED. SO INSTEAD OF DECLARING THIS OUTSIDE AS <this._getPosition();>, WE CAN INLCUDE THIS INSIDE THE CONSTRUCTOR TO EXECUTE IMMEDIATELY.
+    //NOTE: CONSTRUCTOR FUNCTION IS EXECUTED THE MOMENT AN INSTANCE OF APP IS CREATED. SO INSTEAD OF DECLARING THIS OUTSIDE AS <this._getPosition();>, WE CAN INLCUDE THIS INSIDE THE CONSTRUCTOR TO EXECUTE IMMEDIATELY.
+    this._getPosition();
+
+    //EVENTHANDLER WORKOUT TYPE CHANGE
+    //NOTE: EVENTLISTENERS NEEDS TO BE RUN IMMEDIATELY WHEN THEPAGE/APP LOADS SO THATS WHY WE INCLUDE IN THE CONSTRUCTOR FUNCTION OF THE CLASS
+    inputType.addEventListener('change', function () {
+      inputElevation
+        .closest('.form__row')
+        .classList.toggle('form__row--hidden');
+      inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    });
+
+    //EVENTHANDLER WORKOUT SUBMIT FORM
+    form.addEventListener('submit', this._newWorkout.bind(this));
   }
 
   _getPosition() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        this._loadMap.bind(this), //VERY IMPORTANT! we do not invoke it just pass the function name so that we dont use  _loadMap(position) ---- <bind(this)> is used beacuse a call-back function has its no this defined by its nature. In order to assign this , we need to bind it.
+        this._loadMap.bind(this), //VERY IMPORTANT! we do not invoke it just pass the function name so that we dont use  _loadMap(position) ---- <bind(this)> is used because a call-back function has its no this defined by its nature. In order to assign this , we need to bind it.
         //-->GEO API ERROR CALL-BACK FUNCTION
         function () {
           alert('Could not get your location');
@@ -43,7 +56,7 @@ class App {
   }
   _loadMap(position) {
     //-->GEO API SUCCESS CALL-BACK FUNCTION
-    // console.log(position);
+    // console.log(position, this);
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     // console.log(latitude, longitude);
@@ -58,50 +71,44 @@ class App {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
     //EVENTHANDLER BUILT-IN LEAFLET
-    this.#map.on('click', function (lEvent) {
-      //-->SHOW THE FORM UPON CLICK ON THE MAP
-      this.#leafletEvent = lEvent; //Assign as global variable
-      form.classList.remove('hidden'); //reveal the input form
-      inputDistance.focus(); //by default focus on distance
-    });
+    this.#map.on(
+      'click',
+      function (lEvent) {
+        //-->SHOW THE FORM UPON CLICK ON THE MAP
+        this.#leafletEvent = lEvent; //Assign as class private variable
+        form.classList.remove('hidden'); //reveal the input form
+        inputDistance.focus(); //by default focus on distance
+      }.bind(this)
+    );
   }
   _showForm() {}
   _toggleElevationField() {}
-  _newWorkout() {}
+  _newWorkout(e) {
+    e.preventDefault(); //to disable auto submit
+    //-->CLEAR INPUT FIELDS ON THE FORM
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    //-->DISPLAY THE LEAFLET MARKER
+    // console.log(this.#leafletEvent);
+    const { lat, lng } = this.#leafletEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup', //Override custom CSS provided by us on top of the leaflet.css
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
 
-//-->CREATE THE OBJECT
+//-->CREATE THE APP CLASS INSTANCE
 const app = new App();
-
-//EVENTHANDLER WORKOUT SUBMIT FORM
-form.addEventListener('submit', function (e) {
-  e.preventDefault(); //to disable auto submit
-  //-->CLEAR INPUT FIELDS ON THE FORM
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
-  //-->DISPLAY THE LEAFLET MARKER
-  console.log(leafletEvent);
-  const { lat, lng } = leafletEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup', //Override custom CSS provided by us on top of the leaflet.css
-      })
-    )
-    .setPopupContent('Workout')
-    .openPopup();
-});
-
-//EVENTHANDLER WORKOUT TYPE CHANGE
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
