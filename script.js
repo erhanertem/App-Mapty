d68eea6a0bddead161be85dcdd32e3391e761d8e
Erhan Ertem <eertem@gmail.com>
2022-09-23 12:24:07 +0300

    Establish parent and child data classes for the mapty data

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+//APPLICATION ARCHITECTURE
 class App {
   //NOTE: INCORPORATE THE GLOBAL VARIABLES RELATED TO CLASS FUNCTIONS INTO THE CLASS
   #map;
@@ -103,5 +104,51 @@ class App {
   }
 }
 
+//CREATE CLASSES FOR THE MAPTY DATA
+//--> PARENT DATA CLASS
+class Workout {
+  date = new Date();
+  id = String(Date.now()).slice(-10); // id = (new Date() + '').slice(-10)
+
+  constructor(coords, distance, duration) {
+    // this.date =...
+    // this.id = ...
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; //in km
+    this.duration = duration; //in min
+  }
+}
+//-->CHILD DATA CLASSES
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace(); //NOTE: INSERT INTO CONSTRUCTOR FUNCTION TO IMMEDIATELY CALC THE PACE
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance; //min/km
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed(); //NOTE: INSERT INTO CONSTRUCTOR FUNCTION TO IMMEDIATELY CALC THE SPEED
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60); //km/hr
+    return this.speed;
+  }
+}
+
+//TESTING DATA CLASSES
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, cycling1);
+
+//APPLICATION
 //-->CREATE THE APP CLASS INSTANCE
 const app = new App();
